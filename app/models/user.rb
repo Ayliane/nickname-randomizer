08866aee494ee1b86f23ede230381de426af9e73
Ayliane <loviton.vanessa@yahoo.fr>
2020-04-07 13:32:03 +0200

    Just one db call

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   validates :nickname, length: { is: 3 }
 
   def self.generate_nickname
+    already_used_nickname = User.all.pluck(:nickname)
     begin
       nickname = [*('A'..'Z')].sample(3).join
-    end while User.exists?(nickname: nickname)
+    end while already_used_nickname.include?(nickname)
     User.create!(nickname: nickname)
   end
 end
